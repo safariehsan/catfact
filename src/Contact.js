94475cf0fb1ae6004b3877c1b8492b3e089c1d18
safariehsan <ehsansafari@hotmail.com>
2025-07-21 19:09:38 +0200

    modify contact page

--- src/Contact.js
+++ src/Contact.js
@@ -35,12 +35,13 @@ const Contact = () => {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl">
-            Work with us
+            About the developer
           </h2>
           <p className="mt-8 text-pretty text-lg font-medium text-gray-300 sm:text-xl/8">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat.
+            <ul>
+              <li>Email: ehsansafari@hotmail.com</li>
+              <li>LinkedIn: safariehsan</li>
+            </ul>
           </p>
         </div>
       </div>
