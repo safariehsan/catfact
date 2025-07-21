5a9241d0b844945b4fe3fc3c6451990b36ef15ed
safariehsan <ehsansafari@hotmail.com>
2025-07-21 21:05:47 +0200

    fetch cat random image

--- src/List.js
+++ src/List.js
@@ -5,49 +5,7 @@ import { Link } from "react-router";
 
 const List = () => {
   const [facts, setFacts] = useState([]);
-  const products = [
-    {
-      id: 1,
-      name: "Basic Tee",
-      href: "#",
-      imageSrc:
-        "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg",
-      imageAlt: "Front of men's Basic Tee in black.",
-      price: "$35",
-      color: "Black",
-    },
-    {
-      id: 2,
-      name: "Basic Tee",
-      href: "#",
-      imageSrc:
-        "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-02.jpg",
-      imageAlt: "Front of men's Basic Tee in white.",
-      price: "$35",
-      color: "Aspen White",
-    },
-    {
-      id: 3,
-      name: "Basic Tee",
-      href: "#",
-      imageSrc:
-        "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-03.jpg",
-      imageAlt: "Front of men's Basic Tee in dark gray.",
-      price: "$35",
-      color: "Charcoal",
-    },
-    {
-      id: 4,
-      name: "Artwork Tee",
-      href: "#",
-      imageSrc:
-        "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-04.jpg",
-      imageAlt:
-        "Front of men's Artwork Tee in peach with white and brown dots forming an isometric cube.",
-      price: "$35",
-      color: "Iso Dots",
-    },
-  ];
+  const [images, setImages] = useState([]);
   const getFacts = async () => {
     try {
       const response = await axios.get("https://catfact.ninja/facts");
@@ -57,15 +15,29 @@ const List = () => {
       console.error(error);
     }
   };
+  const getCatsImages = async () => {
+    try {
+      const response = await axios.get("https://placekittens.com/g/300/200", {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+      console.log(response);
+      setFacts(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   useEffect(() => {
     getFacts();
+    getCatsImages();
   }, []);
 
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-          Customers also purchased
+          List of Cat Facts
         </h2>
         <div>
           {facts.data && facts.data.length > 0 ? (
