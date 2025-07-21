a3a407a688c0dd52119772549dfeee7afb8ae3d9
safariehsan <ehsansafari@hotmail.com>
2025-07-21 19:33:39 +0200

    modify ui of homepage and contactpage

--- src/404.js
+++ src/404.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router";
 
 const NotFound = () => {
   return (
@@ -12,15 +13,15 @@ const NotFound = () => {
           Sorry, we couldn’t find the page you’re looking for.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="#"
+          <Link
+            to="/"
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Go back home
-          </a>
-          <a href="#" className="text-sm font-semibold text-gray-900">
+          </Link>
+          <Link to="/contact" className="text-sm font-semibold text-gray-900">
             Contact support <span aria-hidden="true">&rarr;</span>
-          </a>
+          </Link>
         </div>
       </div>
     </main>
--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { Link } from "react-router";
 
 const App = () => {
   return (
-    <div className="bg-white">
-      <div className="relative isolate px-6 lg:px-8">
+    <div className="bg-white flex h-screen">
+      <div className="relative isolate px-6 lg:px-8 m-auto">
         <div
           aria-hidden="true"
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -17,7 +17,7 @@ const App = () => {
           />
         </div>
         <div className="mx-auto max-w-2xl">
-          <div className="sm:mb-8 sm:flex sm:justify-center">
+          <div className="mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Technical assignment of Makr Shakr company{" "}
               <a
@@ -31,7 +31,7 @@ const App = () => {
             </div>
           </div>
           <div className="text-center">
-            <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+            <h1 className="text-balance text-4xl font-semibold tracking-tight text-gray-900">
               Welcome to the Cat Fact App
             </h1>
             <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
